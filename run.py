c6847cc5aea79c9816ecdea0280a6dc5d0ba4f90
rubengardner <rgardnercuesta@gmail.com>
2022-09-22 08:38:19 +0000

    Create sales_data fucntion to retrieve info from iser

--- run.py
+++ run.py
@@ -14,4 +14,18 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
-print(data)
+
+
+
+def get_sales_data():
+    """
+    Get sales data
+    """
+
+    print("Please enter sales data")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input('Enter you value here:')
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
